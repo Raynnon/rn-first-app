17b5c9c7db53cf831859aefcecfc32f706baf9ce
Raynnon <assante.f@gmail.com>
2021-07-24 22:52:31 +0200

    added cancel button + a modal when adding goal

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,30 +1,57 @@
 import React, { useState } from "react";
-import { StyleSheet, TextInput, View, Button } from "react-native";
+import {
+  StyleSheet,
+  TextInput,
+  View,
+  TouchableOpacity,
+  Modal,
+  Text,
+  Pressable,
+} from "react-native";
 
-const GoalInput = ({ onAddGoal } = props) => {
+const GoalInput = ({ onAddGoal, modalVisibility, onCancelEdition } = props) => {
   const [enteredGoal, setEnteredGoal] = useState("");
 
   const goalInputHandler = (enteredText) => {
     setEnteredGoal(enteredText);
   };
-
   return (
-    <View style={styles.inputContainer}>
-      <TextInput
-        placeholder="Course Goal"
-        style={styles.input}
-        value={enteredGoal}
-        onChangeText={goalInputHandler}
-      />
-      <Button title="ADD" onPress={() => onAddGoal(enteredGoal)} />
-    </View>
+    <Modal visible={modalVisibility} animationType="slide">
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.input}
+          value={enteredGoal}
+          onChangeText={goalInputHandler}
+        />
+        <View style={styles.buttons}>
+          <Pressable
+            style={styles.goalButtons}
+            android_ripple={{ color: "#B20000" }}
+            onPress={onCancelEdition}
+          >
+            <Text style={styles.buttonTxt}>CANCEL</Text>
+          </Pressable>
+          <Pressable
+            style={[styles.goalButtons, { backgroundColor: "#2196F3" }]}
+            android_ripple={{ color: "#1769AA" }}
+            onPress={() => {
+              onAddGoal(enteredGoal);
+              setEnteredGoal("");
+            }}
+          >
+            <Text style={styles.buttonTxt}>ADD</Text>
+          </Pressable>
+        </View>
+      </View>
+    </Modal>
   );
 };
 
 const styles = StyleSheet.create({
   inputContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
+    flex: 1,
+    justifyContent: "center",
     alignItems: "center",
   },
   input: {
@@ -32,6 +59,23 @@ const styles = StyleSheet.create({
     borderBottomColor: "black",
     borderWidth: 1,
     padding: 10,
+  },
+  buttons: {
+    marginTop: 15,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "60%",
+  },
+  goalButtons: {
+    height: 40,
+    width: 80,
+    padding: 10,
+    backgroundColor: "red",
+    alignItems: "center",
+    elevation: 3,
+  },
+  buttonTxt: {
+    color: "#fff",
   },
 });
 
